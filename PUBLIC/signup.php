295d028b5edb84a78acca2e8af8d66b8f965a17c
vanhioi <149815102+vanhioi@users.noreply.github.com>
2024-03-02 19:55:04 +0700

    Update signup.php

--- PUBLIC/signup.php
+++ PUBLIC/signup.php
@@ -1,40 +1,83 @@
+<?php
+session_start(); // Start session
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $TenKH = $_POST["TenKH"];
+    $username = $_POST["username"];
+    $gmail = $_POST["gmail"];
+    $password = $_POST["password"];
+    $confirm_password = $_POST["confirm_password"];
+
+    // Validate form data (you can add your validation here)
+
+    // Connect to the database
+    $conn = new mysqli("localhost", "root", "", "shopgiay");
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Assuming 'user' role has an ID of 1, adjust this according to your database
+    $role_id = 1;
+
+    // Insert user data into the database
+    $sql = "INSERT INTO user (TenKH, diachiemailKH, password, idrole) VALUES ('$TenKH', '$gmail', '$password', $role_id)";
+    if ($conn->query($sql) === TRUE) {
+        // Redirect to login page after successful signup
+        header("Location: login.php");
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <title> Sign Up</title>
-    
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    </head>
-    <body>
-        <div class="wrapper">
-            <form action="">
-                <h1>
-                    Sign Up
-                    <a href="index.php">
-                        <img style="width: 50px; margin-right: 20px;" src="../IMAGES/logo.png" alt="#">
-                    </a>
-                </h1>
-                <div class="input-box">
-                    <input type="text" placeholder="Username" required>
-                    <i class='bx bxs-user'></i>
-                </div>
-                <div class="input-box">
-                    <input type="Gmail" placeholder="Gmail" required>
-                    <i class='bx bxl-gmail'></i>
-                </div>
-                <div class="input-box">
-                    <input type="password" placeholder="Password" required>
-                    <i class='bx bxs-lock-alt'></i>
-                  </div>
-                  <div class="input-box">
-                    <input type="password" placeholder="Confirm Password" required>
-                    <i class='bx bxs-lock-alt'></i>
-                  </div>
-                  <div>
-                    <button type="submit" class="btn">Submit</button>
-                  </div>
-                  <style>
+<head>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Sign Up</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+<div class="wrapper">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <h1>
+            Sign Up
+            <a href="index.php">
+                <img style="width: 50px; margin-right: 20px;" src="../IMAGES/logo.png" alt="#">
+            </a>
+        </h1>
+        <div class="input-box">
+            <input type="text" name="TenKH" placeholder="TenKH" required>
+            <i class='bx bxs-user'></i>
+        </div>
+        <div class="input-box">
+            <input type="text" name="username" placeholder="Username" required>
+            <i class='bx bxl-gmail'></i>
+        </div>
+        <div class="input-box">
+            <input type="Gmail" name="gmail" placeholder="Gmail" required>
+            <i class='bx bxl-gmail'></i>
+        </div>
+        <div class="input-box">
+            <input type="password" name="password" placeholder="Password" required>
+            <i class='bx bxs-lock-alt'></i>
+        </div>
+        <div class="input-box">
+            <input type="password" name="confirm_password" placeholder="Confirm Password" required>
+            <i class='bx bxs-lock-alt'></i>
+        </div>
+        <div>
+            <button type="submit" class="btn">Submit</button>
+        </div>
+    </form>
+</div>
+<style>
             
             *{
     margin:0;
@@ -128,7 +171,6 @@ body{
     
 
 
-        </style>
-
-    </body>
+        </style>t
+</body>
 </html>
