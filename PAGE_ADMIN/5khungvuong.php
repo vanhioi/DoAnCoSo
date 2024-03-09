6ec848a03680dc6d95a9d4e2956fbde98ff4e929
vanhioi <149815102+vanhioi@users.noreply.github.com>
2024-03-09 14:20:20 +0700

    Update 5khungvuong.php

--- PAGE_ADMIN/5khungvuong.php
+++ PAGE_ADMIN/5khungvuong.php
@@ -3,7 +3,7 @@
 			<a href=""><div class="list_catalog_items trangchu" style="background-color: rgb(50, 50, 50);">Home</div></a>
 			<a href="#"><div class="list_catalog_items danhmuc" style="background-color: rgb(50, 50, 50);">Danh Mục</div></a>
 			<a href="index.php?quanly=danhsachsanpham"><div class="list_catalog_items sanpham" style="background-color: rgb(50, 50, 50);">Sản Phẩm </div></a>
-			<a href=""><div class="list_catalog_items users" style="background-color: rgb(50, 50, 50);">Users</div></a>
+			<a href="user.php"><div class="list_catalog_items users" style="background-color: rgb(50, 50, 50);">Users</div></a>
 			<a href=""><div class="list_catalog_items quanlidonhang" style="background-color: rgb(50, 50, 50);">Quản lí đơn hàng</div></a>
 		</ul>
 	</div>
