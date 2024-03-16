3dcd2a7e5d0b4ee2f130e138a8fe904a16552d4c
NicolasJone <146601910+NicolasJone@users.noreply.github.com>
2024-03-16 12:26:37 +0700

    Update men.php

--- PUBLIC/view/men.php
+++ PUBLIC/view/men.php
@@ -3,72 +3,87 @@ $conn = new mysqli("localhost", "root", "", "shopgiay");
 
 $loaispID = isset($_GET["loaisp"]) ? $_GET["loaisp"] : NULL;
 
-$mensql = "SELECT * FROM sanpham WHERE idLoaisp = 13 ";
+$productsPerPage = 8;
+// Xác định trang hiện tại
+$currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
 
+// Tính toán OFFSET cho truy vấn SQL
+$offset = ($currentPage - 1) * $productsPerPage;
+
+// Truy vấn SQL để lấy dữ liệu phân trang
+$mensql = "SELECT * FROM sanpham WHERE idLoaisp = 13 LIMIT $offset, $productsPerPage";
 $resultMenSql = $conn->query($mensql);
 ?>
 
 <?php
-    if(isset($_GET["idsp"])){
-    
-    if($_GET["idsp"]==13){
-
-    
-    ?>
+if ($resultMenSql->num_rows > 0) {
+?>
     <div class="shop">
-    <div class="shop-hihi">
-        <div class="shop-child">
-        <?php
-            if ($resultMenSql->num_rows > 0) {
-            while ($row = $resultMenSql->fetch_assoc()) {
-                
-        ?>
-                  <a href="index.php?pid=product_detail&id=<?=$row['idSP']?>">
-                  <div class="display-product">
-                        <form action="xulidulieu/addToCart.php" method="post">
-                            <input type="hidden" name="idSP" value="<?php echo $row['idSP']; ?>">
-                            <input type="hidden" name="img" value="<?php echo $row['img']; ?>">
-                            <input type="hidden" name="tenSP" value="<?php echo $row['tenSP']; ?>">
-                            <input type="hidden" name="gia" value="<?php echo $row['gia']; ?>">
-                            <div style="display: flex; justify-content: center;">
-                                <img src="../IMAGES/anhcsld/<?php echo $row['img']; ?>" alt="anh giay">
-                            </div>
-                            <div>
-                                <a href="index.php?pid=product_detail&id=<?=$row['idSP']?>"> <h3 style="margin: 20px 0 20px 0"><?php echo $row['tenSP']; ?></h3> </a>
-                                <p style="margin-bottom: 20px;"><?=number_format($row['gia'], 0, ',', '.')?>vnđ</p>
-                                <a id="<?php echo $row["idSP"]; ?>">
-                                    <button type="submit" name="submit">
-                                        <img style="margin-right: 30px; width: 20px; height: 20px;" src="../IMAGES/Cart.png" alt="">
-                                        Giỏ hàng
-                                    </button>
-                                </a>
-                            </div>
-                        </form>
-                    </div>
-
-                  </a>
-            <?php
+        <div class="shop-hihi">
+            <div class="shop-child">
+                <?php
+                while ($row = $resultMenSql->fetch_assoc()) {
+                ?>
+                    <a href="index.php?pid=product_detail&id=<?= $row['idSP'] ?>">
+                        <div class="display-product">
+                            <form action="#" method="post">
+                                <input type="hidden" name="idSP" value="<?php echo $row['idSP']; ?>">
+                                <input type="hidden" name="img" value="<?php echo $row['img']; ?>">
+                                <input type="hidden" name="tenSP" value="<?php echo $row['tenSP']; ?>">
+                                <input type="hidden" name="gia" value="<?php echo $row['gia']; ?>">
+                                <div style="display: flex; justify-content: center;">
+                                    <img src="../IMAGES/anhcsld/<?php echo $row['img']; ?>" alt="anh giay">
+                                </div>
+                                <div>
+                                    <a href="index.php?pid=product_detail&id=<?= $row['idSP'] ?>">
+                                        <h3 style="margin: 20px 0 20px 0"><?php echo $row['tenSP']; ?></h3>
+                                    </a>
+                                    <p style="margin-bottom: 20px;"><?= number_format($row['gia'], 0, ',', '.') ?>vnđ</p>
+                                    <a id="<?php echo $row["idSP"]; ?>">
+                                        <button type="submit" name="submit">
+                                            <img style="margin-right: 30px; width: 20px; height: 20px;" src="../IMAGES/Cart.png" alt="">
+                                            Giỏ hàng
+                                        </button>
+                                    </a>
+                                </div>
+                            </form>
+                        </div>
+                    </a>
+                <?php
                 }
-            } else {
-                echo "No products found.";
-            }
-            ?>
+                ?>
+            </div>
         </div>
     </div>
-</div>
-
 <?php
-}}
+} else {
+    echo "No products found.";
+}
 ?>
 
+<!-- Hiển thị phân trang -->
+<div class="pagination">
+    <?php
+    $sql = "SELECT COUNT(*) AS total FROM sanpham WHERE idLoaisp = 13";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $totalPages = ceil($row["total"] / $productsPerPage);
+
+    echo "<a href='index.php?idsp=13&page=1'>Trang đầu</a>";
+
+    for ($i = 1; $i <= $totalPages; $i++) {
+        echo "<a href='index.php?idsp=13&page=$i'>$i</a>";
+    }
+
+    echo "<a href='index.php?idsp=13&page=$totalPages'>Trang cuối</a>";
+    ?>
+</div>
 
 <style>
-    
     .shop {
         display: flex;
         justify-content: center;
         margin: 50px 0 50px 0px;
-        
     }
 
     .shop-hihi {
@@ -81,18 +96,18 @@ $resultMenSql = $conn->query($mensql);
     .shop-child {
         display: flex;
         flex-wrap: wrap;
-        justify-content: space-between; /* điều chỉnh theo nhu cầu */
+        justify-content: space-between;
     }
 
     .display-product {
-        width: 23%; /*  Điều chỉnh chiều rộng để vừa 4 mục trong một hàng */
+        width: 23%;
         margin: 10px 0;
         box-sizing: border-box;
     }
 
     .display-product img {
         width: 100%;
-        height: auto; /* Để chiều cao điều chỉnh tự động */
+        height: auto;
         object-fit: cover;
     }
 
@@ -113,5 +128,24 @@ $resultMenSql = $conn->query($mensql);
     a {
         text-decoration: none;
         color: black;
+    }
+
+    .pagination {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+
+    .pagination a {
+        padding: 5px 10px;
+        margin: 0 5px;
+        border: 1px solid #ccc;
+        text-decoration: none;
+        color: black;
+    }
+
+    .pagination a.active {
+        background-color: #ccc;
     }
 </style>
